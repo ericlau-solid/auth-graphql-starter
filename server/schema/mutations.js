cf7054a66357ad3e67302a47b1a51d64dd5788df
Eric Lau <eric.lau@adaptechsolutions.ca>
2017-05-20 23:28:18 -0700

    to lecture 81

---
+++ server/schema/mutations.js
@@ -0,0 +1,22 @@
+const grpahql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+} = graphql;
+const UserType = require('./types/user_type');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, args, request) {
+
+      }
+    }
+  }
+});
